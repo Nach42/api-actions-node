4e48e36e3fc3a24907a85f75ba788b745ae26a66
Nacho Dones <ignacio.dones@alu.uclm.es>
2018-11-05 17:42:54 +0100

    Intents_Spanish

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const webhook = require('./webhook.js');
 const Promise = require('promise');
-const i18n = require('i18n');
 const {
     actionssdk,
     MediaObject,
@@ -36,7 +35,7 @@ app.intent('actions.intent.MAIN', conv => {
 app.intent('actions.intent.SIGN_IN', (conv, input, signin) => {
     if (signin.status === 'OK') {
         const payload = conv.user.profile.payload;
-        conv.ask(i18n.__(`Hi ${payload.name}. What do you want to do next?`))
+        conv.ask(`Hi ${payload.name}. What do you want to do next?`)
     } else {
         conv.ask(`I won't be able to save your data, but what do you want to do next?`)
     }
